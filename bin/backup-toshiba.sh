500481ed6bcc4e08a1aa2626e6fa676127255239
Joe Legner <joelegner.gmail.com>
2020-11-24 16:01:31 -0500

    Migrate backup-toshiba.sh to MacOS

--- bin/backup-toshiba.sh
+++ bin/backup-toshiba.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-# Get current quarter
-MONTH=$(date '+%m')
-QUARTER='Q1'
-if (( 10#$MONTH > 3 )); then QUARTER='Q2'; fi
-if (( 10#$MONTH > 6 )); then QUARTER='Q3'; fi
-if (( 10#$MONTH > 9 )); then QUARTER='Q4'; fi
-
-echo $QUARTER
-
 # Back up Dropbox to external Toshiba drive
-rsync -avX --delete /media/family/GoogleOne /run/media/joe/TOSHIBA\ EXT/backups/GoogleOne/$QUARTER/
-
+rsync -av --delete /Users/joelegner/Google\ Drive /Volumes/TOSHIBA\ EXT/backups/
